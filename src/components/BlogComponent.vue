4c274432622d2257894570a863cafac2e2609b97
AjayAlle <ajayalle10@gmail.com>
2024-12-02 01:52:58 -0500

    first commit

---
+++ src/components/BlogComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-for="blog of blogs" :key="blog.id" >
+
+     
+    <SubBlogComponent :blog=blog />
+
+  </div>
+
+  
+</template>
+
+<script>
+import SubBlogComponent from './SubBlogComponent.vue';
+
+export default {
+  name: "BlogComponent",
+  props: {
+   blogs: Array
+  },
+  components:{
+    SubBlogComponent
+  },
+  data() {
+    return {
+      // Extended description for more content
+      extendedDescription: "Read more to discover insightful details about the topic and why it matters in today's context.",
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
